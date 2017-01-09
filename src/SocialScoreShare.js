c75b602934e54388a3d0d2926d91a8d1e2cd6d20
Jeril Sebastian <seb988@gmail.com>
2017-01-09 13:23:07 +0530

    fb share fix

--- src/SocialScoreShare.js
+++ src/SocialScoreShare.js
@@ -4,7 +4,14 @@ function socialScoreShare() {
 
     caption = 'Flappy Chicken';
     title = chicken.score + ' points!!!';
-    description = 'I just scored ' + chicken.score + ' points on Flappy Chicken. Can you beat it? #flappychicken';
+    description = 'I just scored ' + chicken.score + ' points on Flappy Chicken. Can you beat it?';
 
-    window = window.open('https://www.facebook.com/dialog/share?app_id=9865734934773434&href=https://jerilseb.github.io/flappy-chicken/&picture=https://jerilseb.github.io/flappy-chicken/src/sprites/thumbnail.png&title=' + title + '&caption=' + caption + '&description=' + description + '&hashtag=#flappychicken', '_blank', 'width=250, height=250, top=' + top + ', left=' + left);
+    FB.ui({
+        method: 'share',
+        href: 'https://jerilseb.github.io/flappy-chicken/',
+        quote: description,
+        hashtag: '#flappychicken'
+      }, function(response){});
+
+    //window = window.open('https://www.facebook.com/dialog/share?app_id=1075842552518705&href=https://jerilseb.github.io/flappy-chicken/&picture=https://jerilseb.github.io/flappy-chicken/src/sprites/thumbnail.png&title=' + title + '&caption=' + caption + '&description=' + description + '&hashtag=#flappychicken', '_blank', 'width=250, height=250, top=' + top + ', left=' + left);
 }
